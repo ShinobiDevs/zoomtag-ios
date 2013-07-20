ba2c6ac850e298550604349afcbd1229106743f5
Miki Bergin <shimi.b@gmail.com>
2013-07-20 17:26:52 +0300

    Initial Commit

---
+++ ZoomTag/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZoomTag
+//
+//  Created by Miki Bergin on 7/20/13.
+//  Copyright (c) 2013 Shinobi Devs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZoomTag/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ZoomTag
+//
+//  Created by Miki Bergin on 7/20/13.
+//  Copyright (c) 2013 Shinobi Devs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
